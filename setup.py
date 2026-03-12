e1864f4c39b90a21283ee6c9a3006f7b9adc8fb3
Vo Phuc Ai Tam <s240209@th-ab.de>
2026-03-12 02:14:57 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Phuc Ai Tam Vo",
+    author_email="aitam.workfr@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
